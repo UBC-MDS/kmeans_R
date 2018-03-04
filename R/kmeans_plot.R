832656ecdc1c44ceaa09fd0b652535fd874bf640
CHARLEY CARRIERO <carriero.char@gmail.com>
2018-03-03 17:44:26 -0800

    added unit testing for plotting

---
+++ R/kmeans_plot.R
@@ -0,0 +1,15 @@
+#` Plot data according to cluster assigments
+#`
+#`
+#` @param data -- the data object (data frame or matrix) that k-means clustering has been applied to
+#` @param clust_assigns -- cluster assignments that correspond to each data point
+#` @return plot -- a scatter plot of the data coloured by cluster assignment
+#`
+#` example:
+#` kmeans_plot(data = data, clust_assigns = clusters)
+#`
+
+kmeans_plot <- function(data, clust_assigns) {
+
+}
+
---
+++ tests/testthat/test_kmeans_plot.R
@@ -0,0 +1,17 @@
+library(kmeansR)
+context("kmeans plotting")
+
+test_that("kmeans_plot successfully plots the data and cluster assignments", {
+
+  # test that the correct error is thrown in the absence of data
+  expect_error(kmeans_plot(data = NULL),
+               "Data object is missing or in the wrong format. Make sure you input a matrix or data frame data object")
+
+
+  # test that the correct error is thrown in the absense of cluster assignemnts
+  expect_error(kmeans_plot(data = data.frame(), clust_assigns = NULL),
+               "Cluster assignements are in the wrong format. Make sure you input a list")
+
+  # test that an image file is produced
+  expect_true(file.exists(file.path(system.file("images", package="kmeansR"), "kmeans_plot.png")))
+})
