347195a931414d5ad1858097934a40c7ebd79ec4
bradleypick <bradleypick@gmail.com>
2018-03-08 15:05:19 -0800

    add test for failure to converge

--- tests/testthat/test_kmeans_cluster.R
+++ tests/testthat/test_kmeans_cluster.R
@@ -6,6 +6,7 @@
 #  - checks that returns object of type list
 #  - checks that the list contains the original data
 #  - checks that the list contains the cluster assignments
+#  - checks that warning is thrown when fails to converge
 
 library(kmeansR)
 context("kmeans clustering")
@@ -13,8 +14,13 @@ context("kmeans clustering")
 test_that("kmeans_cluster is a list containing data and cluster assignments", {
 
  # test that the correct error is thrown in the absence of data
- expect_error(kmeans_cluster(data = NULL, centers=NULL),
+ expect_error(kmeans_cluster(data = NULL,
+                             centers=data.frame(x=c(1,5),y=c(2,-5),z=c(3,0))),
                "Data object is missing or in the wrong format. Make sure you input a matrix or data frame data object")
+ # test that the correct error is thrown in the absence of centers
+ expect_error(kmeans_cluster(data = data.frame(x=c(1,5),y=c(2,-5),z=c(3,0)),
+                             centers=NULL),
+              "Centers object is missing or in the wrong format. Make sure you input a matrix or data frame data object")
 
 
  # test that we are actually getting a list
@@ -31,6 +37,13 @@ test_that("kmeans_cluster is a list containing data and cluster assignments", {
  expect_equal(kmeans_cluster(data = data.frame(x = c(1,5,1), y = c(2,-5,2), z = c(3,0,3)),
                           centers = data.frame(x=c(1,5),y=c(2,-5),z=c(3,0)))$assignments,
                           data.frame(assignments=c(1,2,1)))
+
+
+ # testing when we exceed maximum number of iterations
+ expect_warning(kmeans_cluster(data = data.frame(x = c(1,5,1), y = c(2,-5,2), z = c(3,0,3)),
+                               centers = data.frame(x=c(1,5),y=c(2,-5),z=c(3,0)),
+                               max_iter=1),
+                "Did not converge in specified number of iterations")
 
 
 })
