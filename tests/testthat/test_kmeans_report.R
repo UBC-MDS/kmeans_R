758538244551a5bedff8de39ff578397c35c01f9
CHARLEY CARRIERO <carriero.char@gmail.com>
2018-03-08 15:18:02 -0800

    added one test for list length

--- tests/testthat/test_kmeans_report.R
+++ tests/testthat/test_kmeans_report.R
@@ -18,16 +18,20 @@ test_that("kmeans_plot successfully plots the data and cluster assignments", {
   expect_error(kmeans_report(data = data.frame(), clust_assigns = NULL),
                "Cluster assignements are in the wrong format. Make sure you input a data frame")
 
+  # test that correct error is thrown when data & cluster assignments are different lengths
+  expect_error(kmeans_report(data = data.frame(x = c(1,5,1), y = c(2,-5,2), z = c(3,0,3)),
+                             clust_assigns = data.frame(x = c(1), y = c(2))),
+               "Data and cluster assignmenments must be the same length!")
+
   # test that the output is a list ()
   expect_equal(is.list(kmeans_report(data = data.frame(x = c(1,5,1), y = c(2,-5,2), z = c(3,0,3)),
-                                     clust_assigns = data.frame(x = c(1), y = c(2), z = c(3)))), TRUE)
+                                     clust_assigns = data.frame(x = c(1,2,3)))), TRUE)
 
   # check that a plot is produced for 2 dimensional data
   expect_equal(is.ggplot(kmeans_report(data = data.frame(x = c(1,5), y = c(2,-5)),
-                                       clust_assigns = data.frame(x = c(1), y = c(2)))$plot), TRUE)
+                                       clust_assigns = data.frame(x = c(1,2)))$plot), TRUE)
 
   # check that a tibble is returned with summary data
   expect_equal(is.tibble(kmeans_report(data = data.frame(x = c(1,5), y = c(2,-5), z = c(3,0)),
-                                           clust_assigns = data.frame(x = c(1), y = c(2), z = c(3)))$summary), TRUE)
-
+                                       clust_assigns = data.frame(x = c(1,2)))$summary), TRUE)
 })
